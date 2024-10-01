949b9045c03601252e7c9a540875461b2b6c3c04
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-01 18:35:22 +0200

    Created FakeGenerator with method to generate fake produkts, as many as the parameter

---
+++ src/main/java/com/szabolcs/SpringbootWebshop/Utils/FakerGenerator.java
@@ -0,0 +1,38 @@
+package com.szabolcs.SpringbootWebshop.Utils;
+
+import com.github.javafaker.Faker;
+import com.szabolcs.SpringbootWebshop.Model.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@Component
+public class FakerGenerator {
+
+    private static final Faker huFaker = new Faker(new Locale("hu-HU"));
+
+    public static List<Product> createProduct(int amount) {
+        List<Product> products = new ArrayList<>();
+        for (int i = 1; i < amount; i++) {
+            Product product = new Product();
+            product.setId(i);
+            product.setName(huFaker.commerce().productName());
+            product.setPrice(huFaker.number().randomDouble(2,1,1000));
+            product.setDescription(huFaker.lorem().sentence(10));
+            products.add(product);
+        }
+        return products;
+    }
+
+//    public static List<Product> createDummyProducts(int amount) {
+//        List<Product> products = new ArrayList<>();
+//        for (int i = 0; i < amount; i++) {
+//            Product entity = new Product(faker.commerce().productName(), faker.lorem().sentence(4), faker.number().randomDouble(2, 100, 1000));
+//            entity.getTags().add(faker.commerce().material());
+//            products.add(entity);
+//        }
+//        return products;
+//    }
+}
