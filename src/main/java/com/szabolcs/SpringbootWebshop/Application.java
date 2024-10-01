94cb1ec435e521d51b25790a12d5e602dbf7011e
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-01 13:35:44 +0200

    Changed the main App name from SpringbootWebshop to Application. A basic package structure created and an initial basic Product model.

--- src/main/java/com/szabolcs/SpringbootWebshop/Application.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootWebshopApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootWebshopApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
