8015bdfaaedf9cced81e0af4a86c0bd4cc62ee82
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-01 12:49:47 +0200

    Initial commit

---
+++ src/main/java/com/szabolcs/SpringbootWebshop/SpringbootWebshopApplication.java
@@ -0,0 +1,13 @@
+package com.szabolcs.SpringbootWebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szabolcs/SpringbootWebshop/SpringbootWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szabolcs.SpringbootWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
