0f1009f3e9b7542eaa360bcff3d0175a6e52f5c3
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-02 15:32:24 +0200

    Added handleException(ProductNotFoundException exception) method

--- src/main/java/com/szabolcs/SpringbootWebshop/ExceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/ExceptionHandler/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.szabolcs.SpringbootWebshop.ExceptionHandler;
 
+import com.szabolcs.SpringbootWebshop.ExceptionHandler.Exceptions.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler()
+    public ResponseEntity<ErrorResponse> handleException(ProductNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), exception.getMessage(), LocalDateTime.now().toString());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
 }
