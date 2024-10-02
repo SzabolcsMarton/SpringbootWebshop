84b17ce1249aac340ff227d86896c122ad949a6b
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-02 15:12:45 +0200

    Changed Product to Optional<Product> in get and create method

--- src/main/java/com/szabolcs/SpringbootWebshop/Service/IProductService.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Service/IProductService.java
@@ -1,15 +1,17 @@
 package com.szabolcs.SpringbootWebshop.Service;
 
+import com.szabolcs.SpringbootWebshop.Dto.ProductDto;
 import com.szabolcs.SpringbootWebshop.Model.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IProductService {
 
     List<Product> getAllProducts();
-    Product getProductById(int id);
-    Product createProduct(Product newProduct);
-    void updateProduct(Product upDatedProduct, int id);
-    void deleteProduct(int id);
+    Optional<Product> getProductById(long id);
+    Optional<Product> createProduct(ProductDto productDto);
+    void updateProduct(ProductDto productDto, long id);
+    void deleteProduct(long id);
 
 }
