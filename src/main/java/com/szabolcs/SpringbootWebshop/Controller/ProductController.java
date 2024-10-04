555ce41da45030329ab9bb111c819de1041494e7
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-04 15:49:11 +0200

    changed return type in some method

--- src/main/java/com/szabolcs/SpringbootWebshop/Controller/ProductController.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Controller/ProductController.java
@@ -2,7 +2,7 @@ package com.szabolcs.SpringbootWebshop.Controller;
 
 import com.szabolcs.SpringbootWebshop.Dto.ProductDto;
 import com.szabolcs.SpringbootWebshop.Model.Product;
-import com.szabolcs.SpringbootWebshop.Service.Productservice;
+import com.szabolcs.SpringbootWebshop.Service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +14,11 @@ import java.util.Optional;
 @RequestMapping("/api/products")
 public class ProductController {
 
-    private final Productservice productservice;
+    private final ProductService productservice;
 
-     public ProductController(Productservice productservice) {
-         this.productservice = productservice;
-     }
+    public ProductController(ProductService productservice) {
+        this.productservice = productservice;
+    }
 
     @GetMapping
     public ResponseEntity<List<Product>> getAllProducts() {
@@ -27,19 +27,19 @@ public class ProductController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable int id) {
-        Optional<Product>product = productservice.getProductById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(product.get());
+        Product product = productservice.getProductById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 
     @PostMapping
     public ResponseEntity<Product> addEmployee(@RequestBody ProductDto productDto) {
-        Optional<Product> product = productservice.createProduct(productDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(product.get());
+        Product product = productservice.createProduct(productDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(product);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Void> updateProduct(@RequestBody ProductDto productDto, @PathVariable long id) {
-         productservice.updateProduct(productDto, id);
+        productservice.updateProduct(productDto, id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
