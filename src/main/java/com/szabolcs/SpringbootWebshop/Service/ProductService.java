295ce7cc7ff8cd737a2cd5614dcfee3d2d0a6198
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-08 11:50:43 +0200

    added PagedResourcesAssembler<Product> attribute and to constructor method, Added two new method for paginating and with sorting and paginating

--- src/main/java/com/szabolcs/SpringbootWebshop/Service/ProductService.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Service/ProductService.java
@@ -4,6 +4,13 @@ import com.szabolcs.SpringbootWebshop.Dto.ProductDto;
 import com.szabolcs.SpringbootWebshop.ExceptionHandler.Exceptions.ProductNotFoundException;
 import com.szabolcs.SpringbootWebshop.Model.Product;
 import com.szabolcs.SpringbootWebshop.Repository.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 
@@ -13,10 +20,13 @@ import java.util.Optional;
 @Service
 public class ProductService implements IProductService{
 
-    private final ProductRepository productrepo;
+    private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productrepo) {
-        this.productrepo = productrepo;
+    private final PagedResourcesAssembler<Product> pagedResourcesAssembler;
+
+    public ProductService(ProductRepository productrepo, PagedResourcesAssembler<Product> pagedResourcesAssembler) {
+        this.productRepository = productrepo;
+        this.pagedResourcesAssembler = pagedResourcesAssembler;
     }
 
     public static Product mapProductDtoToProduct(Product product, ProductDto productDto) {
@@ -28,12 +38,12 @@ public class ProductService implements IProductService{
 
     @Override
     public List<Product> getAllProducts() {
-        return productrepo.findAll();
+        return productRepository.findAll();
     }
 
     @Override
     public Product getProductById(long id) {
-        Optional<Product> product = productrepo.findById(id);
+        Optional<Product> product = productRepository.findById(id);
         if(product.isEmpty()){
             throw new ProductNotFoundException("No product found with id :" + id);
         }
@@ -44,37 +54,53 @@ public class ProductService implements IProductService{
     public Product createProduct(ProductDto productDto) {
         Product newProduct = new Product();
         mapProductDtoToProduct(newProduct, productDto);
-        return productrepo.save(newProduct);
+        return productRepository.save(newProduct);
     }
 
     @Override
     public void updateProduct(ProductDto productDto, long id) {
-        Optional<Product> product = productrepo.findById(id);
+        Optional<Product> product = productRepository.findById(id);
         if(product.isEmpty()){
             throw new ProductNotFoundException("No product to update with id :" + id);
         }
-        productrepo.save(mapProductDtoToProduct(product.get(), productDto));
+        productRepository.save(mapProductDtoToProduct(product.get(), productDto));
     }
 
     @Override
     public void deleteProduct(long id) {
-        Optional<Product> product = productrepo.findById(id);
+        Optional<Product> product = productRepository.findById(id);
         if(product.isEmpty()){
             throw new ProductNotFoundException("No product to delete with id :" + id);
         }
-        productrepo.deleteById(id);
+        productRepository.deleteById(id);
+    }
+
+    //not in the interface yet methods
+
+
+    public PagedModel<EntityModel<Product>> gelAllProductPaginated(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        System.out.println(pageable);
+        Page<Product> pageResult = productRepository.findAll(pageable);
+        return pagedResourcesAssembler.toModel(pageResult);
+    }
+
+    public PagedModel<EntityModel<Product>> gelAllProductPaginatedSortedBy(int page, int size, String sortBy) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+        Page<Product> pageResult = productRepository.findAll(pageable);
+        return pagedResourcesAssembler.toModel(pageResult);
     }
 
     public List<Product> getProductsByPriceRange(double min, double max) {
-        List<Product> products = productrepo.findProductByPriceBetween(min, max);
+        List<Product> products = productRepository.findProductByPriceBetween(min, max);
         if(products.isEmpty()){
-            throw new ProductNotFoundException("No products with between price: " + min + " and " + max);
+            throw new ProductNotFoundException("No products between price: " + min + " and " + max);
         }
        return products;
     }
 
     public List<Product> getProductsByNamePart(String namePart) {
-        List<Product> products = productrepo.findByNameContainingIgnoreCase(namePart);
+        List<Product> products = productRepository.findByNameContainingIgnoreCase(namePart);
         if(products.isEmpty()){
             throw new ProductNotFoundException("No products with name: " + namePart);
         }
@@ -82,12 +108,13 @@ public class ProductService implements IProductService{
     }
 
     public List<Product> getProductsByDescriptionPart(String descriptionPart) {
-        List<Product> products = productrepo.findByDescriptionContainingIgnoreCase(descriptionPart);
+        List<Product> products = productRepository.findByDescriptionContainingIgnoreCase(descriptionPart);
         if(products.isEmpty()){
             throw new ProductNotFoundException("No products with description: " + descriptionPart);
         }
         return products;
     }
+
 
 
 }
