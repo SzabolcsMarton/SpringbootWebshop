0050985d0d670e118be83af18092ae4311940221
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-08 11:48:10 +0200

    added findAll(Pageable) method in Repository

--- src/main/java/com/szabolcs/SpringbootWebshop/Repository/ProductRepository.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Repository/ProductRepository.java
@@ -1,12 +1,20 @@
 package com.szabolcs.SpringbootWebshop.Repository;
 
 import com.szabolcs.SpringbootWebshop.Model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.lang.NonNull;
+
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, PagingAndSortingRepository<Product, Long> {
+
+    @Override
+    @NonNull
+    Page<Product> findAll(@NonNull Pageable pageable);
 
     List<Product> findProductByPriceBetween(Double min, Double max);
 
