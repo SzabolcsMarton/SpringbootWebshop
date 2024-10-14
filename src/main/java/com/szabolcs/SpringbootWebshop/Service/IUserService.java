f1be56d17f3eccb6a9588119994650ff8daa945b
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-14 16:00:21 +0200

    changed the parameter type to registerdto

--- src/main/java/com/szabolcs/SpringbootWebshop/Service/IUserService.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Service/IUserService.java
@@ -1,5 +1,6 @@
 package com.szabolcs.SpringbootWebshop.Service;
 
+import com.szabolcs.SpringbootWebshop.Dto.RegisterUserDto;
 import com.szabolcs.SpringbootWebshop.Dto.UserDto;
 import com.szabolcs.SpringbootWebshop.Model.User;
 
@@ -9,7 +10,7 @@ public interface IUserService {
 
     List<User> getAllUser();
     User getUserById(long id);
-    User registerUser(UserDto user);
+    User registerUser(RegisterUserDto user);
     void updateUser(UserDto user, long id);
     void deleteUser(long id);
 
