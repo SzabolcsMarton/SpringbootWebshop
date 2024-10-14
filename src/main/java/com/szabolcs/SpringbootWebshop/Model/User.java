5b50fa0aa76f6058ade1ae8382cef3e16cc80308
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-14 15:57:35 +0200

    added password and roles to user and getters/setters

--- src/main/java/com/szabolcs/SpringbootWebshop/Model/User.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Model/User.java
@@ -6,6 +6,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "user")
@@ -20,6 +22,10 @@ public class User {
     private String lastName;
     private String email;
     private String address;
+    private String password;
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = { @JoinColumn(name = "role_id") })
+    private Set<Role> roles = new HashSet<>();
     @Column(name = "created_at")
     @CreatedDate
     private LocalDateTime createdAt;
@@ -78,6 +84,42 @@ public class User {
         this.firstName = firstName;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public void addRole(Role role) {
+        this.roles.add(role);
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(LocalDateTime lastModified) {
+        this.lastModified = lastModified;
+    }
+
     @Override
     public String toString() {
         return "User{" +
@@ -86,6 +128,10 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 ", email='" + email + '\'' +
                 ", address='" + address + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles.toString() +
+                ", createdAt=" + createdAt +
+                ", lastModified=" + lastModified +
                 '}';
     }
 }
