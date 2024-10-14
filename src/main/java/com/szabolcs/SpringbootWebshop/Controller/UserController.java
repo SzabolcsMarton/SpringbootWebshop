f0da93058af4380c3a411641e9d8e2c4390efbc5
SzabolcsMarton <maszabolcs@gmail.com>
2024-10-14 15:57:55 +0200

    added some comment

--- src/main/java/com/szabolcs/SpringbootWebshop/Controller/UserController.java
+++ src/main/java/com/szabolcs/SpringbootWebshop/Controller/UserController.java
@@ -1,5 +1,6 @@
 package com.szabolcs.SpringbootWebshop.Controller;
 
+import com.szabolcs.SpringbootWebshop.Dto.RegisterUserDto;
 import com.szabolcs.SpringbootWebshop.Dto.UserDto;
 import com.szabolcs.SpringbootWebshop.Model.User;
 import com.szabolcs.SpringbootWebshop.Service.UserService;
@@ -26,10 +27,10 @@ public class UserController {
     public User getUserById(@PathVariable long id) {
         return userService.getUserById(id);
     }
-
+    //Ide majd kell tenni security - role - admint
     @PostMapping("/register")
-    public User registerUser(@RequestBody UserDto user) {
-        return userService.registerUser(user);
+    public User createUser(@RequestBody RegisterUserDto regDto) {
+        return userService.registerUser(regDto);
     }
 
     @PutMapping("/{id}")
